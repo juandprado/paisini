a68ee2914b95248a59bf9d7fa056cae3041ecc52
Juan Diego Prado <juandipra@gmail.com>
2022-01-24 17:31:05 -0500

    Landing page finished, initial info page created

--- paisini/src/app/app.module.ts
+++ paisini/src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { InfoComponent } from './info/info.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'info', component: InfoComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
+  declarations: [AppComponent, InfoComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
